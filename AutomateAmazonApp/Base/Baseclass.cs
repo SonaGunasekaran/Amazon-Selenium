92d0598625d8e7984b6158e9089804c16dbd91f3
[Sona] <sona.g@tvsnext.io>
2021-09-20 15:17:14 +0530

    [Sona]Modified: Implementing Extent report

--- AutomateAmazonApp/Base/Baseclass.cs
+++ AutomateAmazonApp/Base/Baseclass.cs
@@ -3,7 +3,8 @@
  * Author:Sona G
  * Date :15/09/2021
  */
-using AutomateAmazonApp.DoAction;
+using AutomateAmazonApp.ExtentReport;
+using AventStack.ExtentReports;
 using log4net;
 using log4net.Config;
 using NUnit.Framework;
@@ -19,6 +20,10 @@ namespace AutomateAmazonApp.Base
         public static ILog logger = LogManager.GetLogger(typeof(Tests));
         public static IWebDriver driver;
 
+        //Exptend report class
+        ExtentReports reports = ReportClass.report();
+        ExtentTest test;
+
         [SetUp]
         public void Setup()
         {
@@ -26,22 +31,29 @@ namespace AutomateAmazonApp.Base
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
+            test = reports.CreateTest("Tests");
+            test.Log(Status.Info, "Automation FaceBook");
+
             //Launching chrome browser
             logger.Info("Launching browser");
             
             driver = new ChromeDriver();
 
             //Maximize the facebook window
-            //driver.Manage().Window.Maximize();
+            driver.Manage().Window.Maximize();
 
             //Get the Amazon URL
             driver.Url = "https://www.amazon.in/";
+
+            //driver.Url = "https://www.amazon.in/ap/signin?_encoding=UTF8&openid.assoc_handle=inflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1";
 
             logger.Info("Exit");
         }
         [TearDown]
         public void TearDown()
         {
+            test.Log(Status.Pass, "Test Passes");
+            reports.Flush();
             //Close the current window
             driver.Quit();
         }
--- AutomateAmazonApp/UnitTest1.cs
+++ AutomateAmazonApp/UnitTest1.cs
@@ -4,35 +4,53 @@
  * Date :15/09/2021
  */
 using NUnit.Framework;
+using NUnit.Framework.Internal;
+using OpenQA.Selenium;
 
 namespace AutomateAmazonApp
 {
     public class Tests : Base.Baseclass
     {
         string csvFilePath = @"C:\Users\sona.g\source\repos\AutomateAmazonApp\AutomateAmazonApp\CsvFile\Filecsv.csv";
-        [Test]
+        [Test, Order(1)]
         public void TestLoginPage()
         {
             DoAction.DoActions.CheckEmailAndPassword(csvFilePath,
                                                      "Email,Password");
         }
 
-        [Test]
+        [Test, Order(2)]
         public void TestSignupPage()
         {
             ActionSigup.SignupActionCass.CheckInputFields();
         }
 
-        [Test]
+        [Test, Order(3)]
         public void TestSearchActionAndBuyNowAction()
         {
             SearchAction.SearchActionClass.CheckInputFields();
         }
 
-        [Test]
+        [Test, Order(4)]
         public void TestAddToCart()
         {
             AddToCartAction.AddToCartActionPage.CheckInputFields();
+        }
+
+        [Test, Order(5)]
+        public void TestAddAddress()
+        {
+            AddressAction.AddressActionClass.CheckInputFields();
+        }
+
+
+        [Test, Order(6)]
+        public void TestSendKeys()
+        {
+            IWebElement MyElement = driver.FindElement(By.Id("twotabsearchtextbox"));
+            MyElement.SendKeys(Keys.NumberPad5);
+            MyElement.SendKeys(Keys.Control + "v");
+            MyElement.SendKeys(Keys.Enter);
         }
     }
 }
