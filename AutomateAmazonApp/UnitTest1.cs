a9adbc214c45e65a0d9200bba044b3eeabfdbc02
[Sona] <sona.g@tvsnext.io>
2021-10-04 16:50:41 +0530

    [Sona]  Modified: Validated end to end flow in amazon

--- AutomateAmazonApp/UnitTest1.cs
+++ AutomateAmazonApp/UnitTest1.cs
@@ -16,59 +16,59 @@ namespace AutomateAmazonApp
         string addressFilePath = @"C:\Users\sona.g\source\repos\AutomateAmazonApp\AutomateAmazonApp\FileCSV\Address.csv";
 
 
-        [Test, Order(1)]
-        public void TestSignupPage()
-        {
-            DoAction.SignupActionCass.CheckInputFields(signupFilePath,
-                                                            "Your name,Mobile Number,Email,Password");
-        }
-        [Test, Order(2)]
+        //[Test, Order(1)]
+        //public void TestSignupPage()
+        //{
+        //    DoAction.DoActions.CreateAmazonAccount(signupFilePath,
+        //                                                "Your name,Mobile Number,Email,Password");
+        //}
+        //[Test, Order(2)]
 
-        public void TestLoginPage()
-        {
+        //public void TestLoginPage()
+        //{
 
-            DoAction.DoActions.CheckEmailAndPassword(csvFilePath,
-                                                     "Email,Password");
-            DoAction.DoActions.TitleAfterLaunching();
+        //    DoAction.DoActions.LoginIntoAamzon(csvFilePath,
+        //                                             "Email,Password");
+        //    DoAction.DoActions.TitleAfterLaunching();
 
-        }
+        //}
 
-        [Test, Order(3)]
-        public void TestBuyNowAction()
-        {
-            DoAction.ProductActionClass.CheckInputFields();
-        }
+        //[Test, Order(3)]
+        //public void TestBuyNowAction()
+        //{
+        //    DoAction.DoActions.CheckProduct();
+        //}
 
-        [Test, Order(4)]
-        public void TestAddToCart()
-        {
-            DoAction.AddToCartActionPage.CheckInputFields();
-        }
+        //[Test, Order(4)]
+        //public void TestAddToCart()
+        //{
+        //    DoAction.DoActions.CheckInputFields();
+        //}
 
-        [Test, Order(5)]
-        public void TestAddAddress()
-        {
-            DoAction.AddressActionClass.CheckInputFields(addressFilePath,
-                                                           "Full name,Mobile number,Pin code,Address line1,Address line2,Town/City");
-        }
+        //[Test, Order(5)]
+        //public void TestAddAddress()
+        //{
+        //    DoAction.DoActions.AddAddressIntoAmzon(addressFilePath,
+        //                                                   "Full name,Mobile number,Pin code,Address line1,Address line2,Town/City");
+        //}
 
-        [Test, Order(6)]
-        public void TestPaymentMethod()
-        {
-            DoAction.PaymentActionPage.CheckForPayment();
-        }
+        //[Test, Order(6)]
+        //public void TestPaymentMethod()
+        //{
+        //    DoAction.DoActions.CheckForPayment();
+        //}
 
-        [Test, Order(7)]
-        public void TestForgetPassword()
-        {
-            DoAction.ForgetPasswordAction.CheckInputFields();
-        }
+        //[Test, Order(7)]
+        //public void TestForgetPassword()
+        //{
+        //    DoAction.DoActions.ForgetPassword();
+        //}
 
-        [Test, Order(8)]
-        public void NegativeTestForLoginPage()
-        {
-            NegativeTestCases.NegativeLoginAction.CheckInputFieldsWithInvalidData();
-        }
+        //[Test, Order(8)]
+        //public void NegativeTestForLoginPage()
+        //{
+        //    NegativeTestCases.NegativeLoginAction.CheckInputFieldsWithInvalidData();
+        //}
 
         [Test, Order(9)]
         public void TestPlaceOrderPage()
@@ -76,11 +76,11 @@ namespace AutomateAmazonApp
             Pages.PlaceOrderPage.CheckForPayment();
         }
 
-        [Test, Order(10)]
-        public void TestMethodForEmailSending()
-        {
-            ExtentReport.Email.UserData(emailCsvPath, "From,To,Password");
-        }
+        //[Test, Order(10)]
+        //public void TestMethodForEmailSending()
+        //{
+        //    ExtentReport.Email.UserData(emailCsvPath, "From,To,Password");
+        //}
 
     }
 }
